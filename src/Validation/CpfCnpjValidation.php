15f8ce7e0d1ad7cfc19558cef0025914e15909c5
Elton <elton.inacio95@gmail.com>
2016-03-10 23:16:13 -0300

    update Validation

--- src/Validation/CpfCnpjValidation.php
+++ src/Validation/CpfCnpjValidation.php
@@ -54,21 +54,18 @@ class CpfCnpjValidation extends IlluminateValidator
 	protected function validatecpf($attribute, $value, $parameters)
 	{
 		
-		$cpf = $value;
-	   // Elimina possivel mascara
+	   $cpf = $value;
+	   
 	   $cpf = preg_replace('/[^0-9]/', '', $cpf);
 	   $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
 	    
-	   // sequências invalidas abaixo 
+	   
 	   $sequencia = array('00000000000', '11111111111', '22222222222', '33333333333', '44444444444', '55555555555', '66666666666', '77777777777', '88888888888', '99999999999');
 
-	   // Verifica se o numero de digitos informados é igual a 11 
 	   if (strlen($cpf) != 11) {
 	       return false;
 	   } else if (in_array($cpf, $sequencia)) {
 	       return false;
-	    // Calcula os digitos verificadores para verificar se o
-	    // CPF é válido
 	    } else {   
 	       for ($t = 9; $t < 11; $t++) {
 	           for ($d = 0, $c = 0; $c < $t; $c++) {
@@ -94,101 +91,73 @@ class CpfCnpjValidation extends IlluminateValidator
 	protected function validatecnpj($attribute, $value, $parameters)
 	{
 
-		/*
-	        Etapa 1: Cria um array com apenas os digitos numéricos,
-	        isso permite receber o cnpj em diferentes
-	        formatos como "00.000.000/0000-00", "00000000000000", "00 000 000 0000 00"
-	        etc...
-        */
+
         $cnpj = preg_replace('/\D/', '', $value);
         $num = array();
  
-        /* Cria um array com os valores */
-        for($i=0; $i<(strlen($cnpj)); $i++) {
+        for($i = 0; $i < (strlen($cnpj)); $i++) {
  
             $num[]=$cnpj[$i];
         }
  
-         //Etapa 2: Conta os dígitos, um Cnpj válido possui 14 dígitos numéricos.
-         if(count($num)!=14)
-         {
-             return false;
-         }
-         /*
-         Etapa 3: O número 00000000000 embora não seja um cnpj real resultaria
-         um cnpj válido após o calculo dos dígitos verificares
-         e por isso precisa ser filtradas nesta etapa.
-         */
-         if ($num[0]==0 && $num[1]==0 && $num[2]==0
-             && $num[3]==0 && $num[4]==0 && $num[5]==0
-             && $num[6]==0 && $num[7]==0 && $num[8]==0
-             && $num[9]==0 && $num[10]==0 && $num[11]==0)
-         {
-             return false;
-         }
-         //Etapa 4: Calcula e compara o primeiro dígito verificador.
-         else
-         {
-                $j=5;
-                for($i=0; $i<4; $i++)
-                {
-                    $multiplica[$i]=$num[$i]*$j;
-                    $j--;
-                }
-                $soma = array_sum($multiplica);
-                $j=9;
-                for($i=4; $i<12; $i++)
-                {
-                    $multiplica[$i]=$num[$i]*$j;
-                    $j--;
-                }
-                $soma = array_sum($multiplica);
-                $resto = $soma%11;
-                 if($resto<2)
-                 {
-                     $dg=0;
-                 }
-                 else
-                 {
-                    $dg=11-$resto;
-                 }
-                 if($dg!=$num[12])
-                 {
-                    return false;
-                 }
+        if(count($num) != 14){
+           return false;
         }
-         //Etapa 5: Calcula e compara o segundo dígito verificador.
- 
-         $j=6;
-         for($i=0; $i<5; $i++)
-         {
-             $multiplica[$i]=$num[$i]*$j;
-             $j--;
-         }
-         $soma = array_sum($multiplica);
-         $j=9;
-         for($i=5; $i<13; $i++)
-         {
-            $multiplica[$i]=$num[$i]*$j;
-            $j--;
-         }
-         $soma = array_sum($multiplica);
-         $resto = $soma%11;
-         if($resto<2)
-         {
-            $dg=0;
-         }
-         else
-         {
-            $dg=11-$resto;
-         }
-         if($dg!=$num[13])
-         {
+
+        if ($num[0]==0 && $num[1]==0 && $num[2]==0
+           && $num[3]==0 && $num[4]==0 && $num[5]==0
+           && $num[6]==0 && $num[7]==0 && $num[8]==0
+           && $num[9]==0 && $num[10]==0 && $num[11]==0)
+        {
             return false;
-         }
-         else
-         {
+        }
+        else
+        {
+            $j = 5;
+            for($i = 0; $i < 4; $i++){
+                $multiplica[$i] = $num[$i] * $j;
+                $j--;
+            }
+            $soma = array_sum($multiplica);
+            $j = 9;
+            for($i = 4; $i < 12; $i++){
+                $multiplica[$i] = $num[$i] * $j;
+                $j--;
+            }
+            $soma = array_sum($multiplica);
+            $resto = $soma % 11;
+            if($resto < 2 ){
+                $dg = 0;
+            } else {
+                $dg = 11 - $resto;
+            }
+            if($dg != $num[12]) {
+                return false;
+            }
+        }
+
+        $j = 6;
+        for($i = 0; $i < 5 ; $i++){
+            $multiplica[$i] = $num[$i] * $j;
+            $j--;
+        }
+        $soma = array_sum($multiplica);
+        $j = 9;
+        for($i = 5; $i < 13; $i++){
+            $multiplica[$i] = $num[$i] * $j;
+            $j--;
+        }
+        $soma = array_sum($multiplica);
+        $resto = $soma % 11;
+        if($resto < 2){
+            $dg = 0;
+        } else {
+            $dg = 11 - $resto;
+        }
+        if($dg != $num[13]){
+            return false;
+        } else {
             return true;
-         }
+        }
     }
 }
